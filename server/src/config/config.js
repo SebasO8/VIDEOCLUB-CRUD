5be45b3a4d1fea094012edebb5db8ec671d31122
Rafael Ramírez <rrramirezb251@gmail.com>
2021-09-26 13:28:34 -0500

    client-side

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -1,6 +1,6 @@
 export const config = {
   host: /*process.env.DB_HOST ||*/ 'localhost',
   user: /*process.env.DB_USER ||*/ 'root',
-  password: /*process.env.DB_PASSWORD ||*/ 'sebas2734392',
-  database: /*process.env.DB_DATABASE ||*/ 'movie',
+  password: /*process.env.DB_PASSWORD ||*/ '1465Rafael*',
+  database: /*process.env.DB_DATABASE ||*/ 'movies',
 }
